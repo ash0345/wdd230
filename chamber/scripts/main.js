cb3330e6129265fa73bd75921f26a36487ec49a1
ash0345 <ang21002@byui.edu>
2022-10-08 16:04:55 -1000

    final change today

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
     document.getElementsById("#nav-bar").classList.toggle("open");
+    document.getElementById("#hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("#hamburgerBtn");
