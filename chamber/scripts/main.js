fc3c4a228840ab27ced2eeed1500f905665d0245
ash0345 <ang21002@byui.edu>
2022-10-10 20:31:07 -1000

    lots of work 10/10

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,18 +1,3 @@
 function toggleMenu() {
-    document.getElementsById("#nav-bar").classList.toggle("open");
-    document.getElementById("#hamburgerBtn").classList.toggle("open");
+    document.getElementById("nav-bar").classList.toggle("hide");
 }
-
-const x = document.getElementById("#hamburgerBtn");
-x.onclick = toggleMenu;
-
-
-// const datefieldUK = document.querySelector(".date");
-
-// const now = new Date();
-// const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-// 	dateStyle: "full"
-// }).format(now);
-// // long, medium, short options ... try them
-
-// datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
